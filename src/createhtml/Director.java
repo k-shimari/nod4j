bb8a705242d27aafe640fb67156e68fd565226ec
k-shimari <k-simari@ist.osaka-u.ac.jp>
2019-03-02 19:10:31 +0900

    modify css and refactoring

--- src/createhtml/Director.java
+++ src/createhtml/Director.java
@@ -8,9 +8,10 @@ public class Director {
 	private Builder builder;
 	private static final String OUTPUTDIR="sample/output/";
 	private static final String META="<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />";
-	//TODO JSとCSSのパス指定
+
+	//TODO JSとCSSのパス指定,自動でresourcesから取ってくる設定
 	private static final String[] JSs= {"../../resources/test.js","../../resources/prettify.js",};
-	private final String[] CSSs = {"../../resources/prettify.css","../../resources/custom.css"};
+	private static final String[] CSSs = {"../../resources/prettify.css","../../resources/custom.css"};
 
 
 	// 実際はBuilderのサブクラスを引数に取る
@@ -20,6 +21,21 @@ public class Director {
 
 	// 文章の中身を作る
 	public void construct(SrcFile file) {
+		constructHead(file);
+		builder.makeBody(file.getFilename()+".java");
+		builder.makeHeading("今日の目標");
+		constructCode(file);
+
+		builder.makeHeading("使うもの");
+
+		testadd();
+
+		builder.makeScript("<script>prettyPrint();</script>");
+		// TODO 出力先の指定
+		builder.close(OUTPUTDIR);
+	}
+
+	private void constructHead(SrcFile file) {
 		builder.makeHead(META);
 		builder.makeTitle(file.getFilename()+".java");
 		for(String js: JSs) {
@@ -28,8 +44,9 @@ public class Director {
 		for(String CSS: CSSs) {
 			builder.makeStyle(CSS);
 		}
-		builder.makeBody(file.getFilename()+".java");
-		builder.makeHeading("今日の目標");
+	}
+
+	private void constructCode(SrcFile file) {
 		int index=0;
 		for(String line: file.getLines()) {
 			if(index++==0) {
@@ -39,15 +56,6 @@ public class Director {
 			}
 		}
 		builder.postMakeCode();
-		builder.makeScript("<script>prettyPrint();</script>");
-		builder.makeHeading("使うもの");
-
-
-		testadd();
-
-		// TODO 出力先の指定
-		//builder.close();
-		builder.close(OUTPUTDIR);
 	}
 
 	private void testadd() {
@@ -57,7 +65,6 @@ public class Director {
 				+ "<script>addOption()</script>";
 		builder.makeScript(line);
 
-
 		String beforestr="\"var1\"";
 		String afterstr="var2"+"\r\n";
 		for (int i=0;i<10;i++) {
@@ -65,16 +72,35 @@ public class Director {
 		}
 		String line2="<ul class=\"menu\">\r\n" +
 				"    <li class=\"menu__single\">\r\n" +
-				"        <a href=\"#\" class=\"init-bottom\">var1</a>\r\n" +
+				"        <a href=\"#\">var1</a>\r\n" +
 				"        <ul class=\"menu__second-level\">\r\n" +
-				"            <li><a href=\"#\">21</a></li>\r\n" +
-				"            <li><a href=\"#\">23</a></li>\r\n" +
-				"            <li><a href=\"#\">25</a></li>\r\n" +
+				 getli(21)+
+				 getli(23)+
+				 getli(25)+
+				 getli(27)+
+				 getli("str")+
 				"        </ul>\r\n" +
 				"    </li>\r\n" +
 				"    <!-- 他グローバルナビメニュー省略 -->\r\n" +
 				"</ul>";
 		builder.makeScript(line2);
-
 	}
+
+	private String getli(String val) {
+		return "            <li><a href=\"#\">"+val+"</a></li>\r\n";
+	}
+
+	private String getli(int val) {
+		return "            <li><a href=\"#\">"+Integer.toString(val)+"</a></li>\r\n";
+	}
+
+
+
+
+
+
+
+
+
+
 }
