05928f9bfff0b42ad7ccfab3de51e3fba4c60f2d
k-shimari <k-simari@ist.osaka-u.ac.jp>
2019-03-02 15:30:59 +0900

    js css to resources/

---
+++ resources/test.js
@@ -0,0 +1,10 @@
+
+function addOption() {
+	var select = document.getElementById('sample')
+	var option = document.createElement('option')
+	option.value = "123456"
+	//画面に表示されるテキスト部分は createTextNode で作って、optionの子要素として追加
+	option.appendChild(document.createTextNode('表示用文字列'))
+	//プルダウンに追加
+	select.appendChild(option)
+}
