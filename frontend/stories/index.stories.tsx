aacdfa726fc3329afaa460fad35c154001dc660f
Naoto Ishida <maxfield.walker@gmail.com>
2019-08-03 13:34:04 +0900

    Added storybook

---
+++ frontend/stories/index.stories.tsx
@@ -0,0 +1,5 @@
+import { storiesOf } from '@storybook/react';
+import { HelloWorld } from 'app/components/atoms/helloWorld';
+import * as React from 'react';
+
+storiesOf('Logvis', module).add('Hello, World!!', () => <HelloWorld />);
