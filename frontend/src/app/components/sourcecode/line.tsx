5db365641de81b6b6ebf45be844c9853ca1b6938
Naoto Ishida <maxfield.walker@gmail.com>
2019-08-05 15:01:31 +0900

    Add source code line component

---
+++ frontend/src/app/components/sourcecode/line.tsx
@@ -0,0 +1,31 @@
+import { IToken } from 'chevrotain';
+import * as React from 'react';
+import { Space } from './space';
+import { Token } from './token';
+
+interface Props {
+  line: number;
+  tokens: IToken[];
+}
+
+function f(tokens: IToken[]): React.ReactElement[] {
+  const result: React.ReactElement[] = [];
+  let preEndColumn = 0;
+  for (const token of tokens) {
+    const startColumn = token.startColumn!;
+    const endColumn = token.endColumn!;
+
+    const delta = startColumn - preEndColumn - 1;
+    if (delta > 0) {
+      result.push(<Space length={delta} />);
+    }
+
+    result.push(<Token>{token.image}</Token>);
+
+    preEndColumn = endColumn!;
+  }
+
+  return result;
+}
+
+export const Line: React.FunctionComponent<Props> = (props) => <span>{f(props.tokens)}</span>;
--- frontend/stories/sourceCode.stories.tsx
+++ frontend/stories/sourceCode.stories.tsx
@@ -1,10 +1,11 @@
 import { storiesOf } from '@storybook/react';
 import { Sourcecode } from 'app/components/sourcecode';
+import { Line } from 'app/components/sourcecode/line';
 import { Token } from 'app/components/sourcecode/token';
-import { ILexingResult } from 'chevrotain';
+import { ILexingResult, Lexer } from 'chevrotain';
 import * as React from 'react';
 const { parse } = require('java-parser');
-const JavaLexer = require('java-parser/src/lexer');
+const JavaLexer: Lexer = require('java-parser/src/lexer');
 
 const javaText = `public class HelloWorldExample{
   public static void main(String args[]){
@@ -13,6 +14,8 @@ const javaText = `public class HelloWorldExample{
 }
 `;
 
+const javaText2 = `public static void main(String args[]) {`;
+
 const lexResult: ILexingResult = JavaLexer.tokenize(javaText);
 const cst = parse(javaText);
 console.log(cst);
@@ -20,4 +23,5 @@ console.log(lexResult);
 
 storiesOf('Source code', module)
   .add('Basic', () => <Sourcecode tokens={lexResult.tokens} />)
+  .add('Line', () => <Line line={0} tokens={JavaLexer.tokenize(javaText2).tokens} />)
   .add('Token', () => <Token>public</Token>);
