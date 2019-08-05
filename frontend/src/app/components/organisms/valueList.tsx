45da4d2f8fe2517ad1c3f7d8a159f52a1fb0b9d9
Naoto Ishida <maxfield.walker@gmail.com>
2019-08-05 12:31:41 +0900

    Add styles

--- frontend/src/app/components/organisms/valueList.tsx
+++ frontend/src/app/components/organisms/valueList.tsx
@@ -1,4 +1,6 @@
+import { Paper } from '@material-ui/core';
 import List from '@material-ui/core/List';
+import { makeStyles } from '@material-ui/core/styles';
 import {
   ValueListItem,
   ValueListItemId,
@@ -15,20 +17,29 @@ interface Props {
   onArrowDownwardClick?(id: ValueListItemId): void;
 }
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 300
+  }
+}));
+
 export const ValueList: React.FunctionComponent<Props> = (props) => {
   const { items, onArrowDownwardClick, onArrowUpwardClick } = props;
+  const classes = useStyles();
 
   return (
-    <List dense={false}>
-      {items.map(({ id, value }) => (
-        <ValueListItem
-          key={id}
-          id={id}
-          value={value}
-          onArrowUpwardClick={onArrowUpwardClick}
-          onArrowDownwardClick={onArrowDownwardClick}
-        ></ValueListItem>
-      ))}
-    </List>
+    <Paper className={classes.root}>
+      <List dense={false}>
+        {items.map(({ id, value }) => (
+          <ValueListItem
+            key={id}
+            id={id}
+            value={value}
+            onArrowUpwardClick={onArrowUpwardClick}
+            onArrowDownwardClick={onArrowDownwardClick}
+          ></ValueListItem>
+        ))}
+      </List>
+    </Paper>
   );
 };
