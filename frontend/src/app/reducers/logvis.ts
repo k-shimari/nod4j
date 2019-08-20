26201eadefef5f20c76092d6fcc9ed237cf3c244
Naoto Ishida <maxfield.walker@gmail.com>
2019-08-20 16:32:46 +0900

    Add states relating to value list filter

--- frontend/src/app/reducers/logvis.ts
+++ frontend/src/app/reducers/logvis.ts
@@ -1,6 +1,12 @@
 import { handleActions } from 'redux-actions';
 import { RootState } from './index';
 
-const initialState: RootState.LogvisState = {};
+const initialState: RootState.LogvisState = {
+  filter: {
+    range: {}
+  },
+  originalValueListData: {},
+  filteredValueListData: {}
+};
 
 export const logvisReducer = handleActions<RootState.LogvisState, any>({}, initialState);
