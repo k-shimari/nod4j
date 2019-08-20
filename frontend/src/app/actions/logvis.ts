8727c7c7874911d7b74e9c1dd58d33f2d3ed59d5
Naoto Ishida <maxfield.walker@gmail.com>
2019-08-20 16:24:12 +0900

    Add actions

--- frontend/src/app/actions/logvis.ts
+++ frontend/src/app/actions/logvis.ts
@@ -2,7 +2,15 @@ import { createAction } from 'redux-actions';
 
 export namespace LogvisActions {
   export enum Type {
-    DUMMY_ACTION = 'DUMMY_ACTION'
+    DUMMY_ACTION = 'DUMMY_ACTION',
+
+    // Filter系
+    SET_VALUE_LIST_FILTER = 'SET_VALUE_LIST_FILTER',
+    REMOVE_FILTER = 'REMOVE_FILTER',
+    CLEAR_ALL_FILTERS = 'CLEAR_ALL_FILTERS',
+
+    SET_ORIGINAL_VALUE_LIST_DATA = 'SET_ORIGINAL_VALUE_LIST_DATA',
+    SET_FILTERED_VALUE_LIST_DATA = 'SET_FILTERED_VALUE_LIST_DATA'
   }
 
   export const dummyAction = createAction(Type.DUMMY_ACTION);
