cb3ff4edafc98331796819dbe155e7c888f588a8
Naoto Ishida <maxfield.walker@gmail.com>
2019-08-21 20:02:52 +0900

    Move directory navigation story

--- frontend/stories/fileTable.stories.tsx
+++ frontend/stories/fileTable.stories.tsx
@@ -1,13 +1,16 @@
 import { storiesOf } from '@storybook/react';
-import { FileTable, FileTableRow } from 'app/components/organisms/fileTable';
+import { FileTable } from 'app/components/organisms/fileTable';
+import { PathNavigation } from 'app/components/organisms/pathNavigation';
 import * as React from 'react';
 
-storiesOf('File Table', module).add('Basic', () => (
-  <FileTable
-    data={[
-      { name: 'package.json', author: 'naoto', date: new Date(), kind: 'file' },
-      { name: '.gitignore', author: 'naoto', date: new Date(), kind: 'file' },
-      { name: 'node_modules', author: 'naoto', date: new Date(), kind: 'dir' }
-    ]}
-  ></FileTable>
-));
+storiesOf('File Navigation', module)
+  .add('File Table', () => (
+    <FileTable
+      data={[
+        { name: 'package.json', author: 'naoto', date: new Date(), kind: 'file' },
+        { name: '.gitignore', author: 'naoto', date: new Date(), kind: 'file' },
+        { name: 'node_modules', author: 'naoto', date: new Date(), kind: 'dir' }
+      ]}
+    ></FileTable>
+  ))
+  .add('Path navigation', () => <PathNavigation />);
--- frontend/stories/index.stories.tsx
+++ frontend/stories/index.stories.tsx
@@ -1,8 +1,6 @@
 import { storiesOf } from '@storybook/react';
 import { HelloWorld } from 'app/components/atoms/helloWorld';
-import { PathNavigation } from 'app/components/organisms/pathNavigation';
 import * as React from 'react';
 
 storiesOf('Logvis', module)
   .add('Hello, World!!', () => <HelloWorld />)
-  .add('Path navigation', () => <PathNavigation />);
