b27fdc4ee0ee3f50a147ec713c89912dad16cf5f
k-shimari <k-simari@ist.osaka-u.ac.jp>
2019-08-21 16:12:36 +0900

    add json structure

--- src/createjson/CreateJson.java
+++ src/createjson/CreateJson.java
@@ -1,8 +1,10 @@
 package createjson;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import data.Json;
 import data.Recentdata;
 import data.SeloggerFiles;
 
@@ -17,6 +19,65 @@ public class CreateJson {
 
 	public void start() {
 		System.out.println("Create json ...");
+		Json json = create();
+
+		// @TODO print とか
+		System.out.println(json);
+
+	}
+
+	private Json create() {
+		Json json = new Json();
+		setClassName(json);
+		setMethodName(json);
+		setVar(json);
+		setLinenum(json);
+		setCount(json);
+		setValueList(json);
+		return json;
+	}
+
+	private void setClassName(Json json) {
+		// @TODO
+		String className = "hogehoge.java";
+		json.setClassName(className);
+
+	}
+
+	private void setMethodName(Json json) {
+		// @TODO
+		String methodName = "fuga";
+		json.setMethodName(methodName);
+	}
+
+	private void setVar(Json json) {
+		// @TODO
+		String var = "var";
+		json.setVar(var);
+	}
+
+	private void setLinenum(Json json) {
+		// @TODO
+		int linenum =100;
+		json.setLinenum(linenum);
+
+	}
+
+	private void setCount(Json json) {
+		// @TODO
+		int count = 1;
+		json.setCount(count);
+	}
+
+	private void setValueList(Json json) {
+		// @TODO
+		List<Recentdata> valueList = new ArrayList<Recentdata>();
+
+		json.setValueList(valueList);
+
+	}
+
+	private void getRecentdata() {
 		Map<String, List<Recentdata>> recdatamap = selfiles.getRecentDataMap();
 		recdatamap.keySet()
 				.stream()
@@ -27,6 +88,7 @@ public class CreateJson {
 							t -> System.out.println(t.getData() + "," + t.getThread() + "," + t.getTimestamp()));
 					System.out.println();
 				});
+
 	}
 
 }
---
+++ src/data/Json.java
@@ -0,0 +1,70 @@
+package data;
+
+import java.util.List;
+
+public class Json {
+
+	private String className;
+	private String methodName;
+	private String var;
+	private int linenum;
+	private final String _countcomment = "その行で何回目の出現か(同じ変数が行内にあったときのみカウント+)";
+	private int count;
+	private List<Recentdata> valueList;
+
+	public Json() {
+	}
+
+	public String getClassName() {
+		return className;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+	public String getMethodName() {
+		return methodName;
+	}
+
+	public void setMethodName(String methodName) {
+		this.methodName = methodName;
+	}
+
+	public String getVar() {
+		return var;
+	}
+
+	public void setVar(String var) {
+		this.var = var;
+	}
+
+	public int getLinenum() {
+		return linenum;
+	}
+
+	public void setLinenum(int linenum) {
+		this.linenum = linenum;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public List<Recentdata> getValueList() {
+		return valueList;
+	}
+
+	public void setValueList(List<Recentdata> valueList) {
+		this.valueList = valueList;
+	}
+
+	public String get_countcomment() {
+		return _countcomment;
+	}
+
+}
