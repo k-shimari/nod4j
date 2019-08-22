d1610212688abd5801b254feae7b7424f8e7acb4
k-shimari <k-simari@ist.osaka-u.ac.jp>
2019-08-22 19:58:02 +0900

    delete old files

--- src/data/FieldInfo.java
+++ src/data/FieldInfo.java
@@ -6,10 +6,40 @@ public class FieldInfo {
 	private boolean isPut;
 	private boolean isFail;
 
+	/* length of "FIELDNAME=" and "NAME=" */
+	private static final int FIELDNAMEINDEX = 10;
+	private static final int NAMEINDEX = 5;
+
+	public FieldInfo() {
+	}
+
 	public FieldInfo(String fieldname, boolean isPut, boolean isFail) {
 		this.fieldname = fieldname;
 		this.isPut = isPut;
 		this.isFail = isFail;
+	}
+
+	public FieldInfo (String elemdat[]) {
+		if (elemdat[5].equals("GET_STATIC_FIELD") | elemdat[5].equals("PUT_STATIC_FIELD")) {
+			this.fieldname=elemdat[8].substring(FIELDNAMEINDEX);
+			this.isPut = elemdat[5].contains("PUT");
+			this.isFail=false;
+		} else if (elemdat[5].equals("GET_INSTANCE_FIELD_RESULT")
+				|| elemdat[5].equals("PUT_INSTANCE_FIELD_VALUE")) {
+			this.fieldname = elemdat[9].substring(FIELDNAMEINDEX);
+			this.isPut = elemdat[5].contains("PUT");
+			this.isFail=false;
+		} else if (elemdat[5].equals("LOCAL_STORE") || elemdat[5].equals("LOCAL_LOAD")) {
+			this.fieldname = elemdat[8].substring(NAMEINDEX);
+			this.isPut = elemdat[5].equals("LOCAL_STORE");
+			/*SELoggerの使用で局所変数で名前がないものが取れるので無視*/
+			this.isFail=fieldname.equals("(Unavailable)");
+		} else {
+			/*命令がない時は失敗*/
+			this.fieldname = "";
+			this.isPut = false;
+			this.isFail=true;
+		}
 	}
 
 	public String getFieldname() {
@@ -23,4 +53,8 @@ public class FieldInfo {
 	public boolean getisFail() {
 		return isFail;
 	}
+
+
+
+
 }
--- src/data/SeloggerFiles.java
+++ src/data/SeloggerFiles.java
@@ -4,28 +4,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class SeloggerFiles {
 
 	private List<String> linesRecentdata = new ArrayList<>();
 	private List<String> linesDataids = new ArrayList<>();
 	private List<String> linesMethods = new ArrayList<>();
-
-	/*ファイル・行・変数に対する変数のリスト*/
-	private Map<FileLineDataId, List<String>> linevarMap = new HashMap<>();
-	/*ファイル・行・変数に対する変数・出現回数・のリスト*/
-	private Map<FileLineVarDataId, DataIdVar> linevardetailMap = new HashMap<>();
-
-	private Map<String, String> fileIDMap = new HashMap<>();
-	private Map<String, List<String>> dupfileIDMap = new HashMap<>();
-
-
 	private DataIdMaps dataidMaps;
-
-
 
 	public SeloggerFiles(String dir) {
 		try {
@@ -34,84 +20,14 @@ public class SeloggerFiles {
 			this.linesMethods = Files.readAllLines(Paths.get(dir, "selogger", "methods.txt"));
 			this.dataidMaps = new DataIdMaps();
 			CreateMap();
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
 	private void CreateMap() {
-//		CreateFileIDMap();
 		dataidMaps.createMap(linesDataids, linesMethods,linesRecentdata);
 	}
-
-
-
-	/**methods.txtをもとに，ファイルに対するそのIDを返すMapを作成
-	 *
-	 */
-//	private void CreateFileIDMap() {
-//		for (String line : this.linesMethods) {
-//			String ele[] = line.split(",");
-//			if (ele.length > 6) {
-//				if (fileIDMap.containsKey(ele[6])) {
-//					/* internal class indicates same file, so separate*/
-//					List<String> list = new ArrayList<String>();
-//					if (dupfileIDMap.containsKey(ele[6])) {
-//						list = dupfileIDMap.get(ele[6]);
-//						list.add(ele[0]);
-//						dupfileIDMap.put(ele[6], list);
-//					} else {
-//						list.add(fileIDMap.get(ele[6]));
-//						list.add(ele[0]);
-//						dupfileIDMap.put(ele[6], list);
-//					}
-//				} else {
-//					fileIDMap.put(ele[6], ele[0]);
-//				}
-//			}
-//		}
-//	}
-
-
-
-
-
-//	private void createlinevardetailMap(String fieldname, boolean isPut, String dataid, String fileID,
-//			String linenum) {
-//		fieldIDList.add(dataid);
-//		/* ファイル・行に対する変数のリスト,変数の詳細リストを更新 or 生成*/
-//		if (linevarMap.get(new FileLineDataId(fileID, linenum)) != null) {
-//			/* 変数がすでにその行にあるかどうか確認*/
-//			if (linevardetailMap.containsKey(new FileLineVarDataId(fileID, linenum, fieldname))) {
-//				DataIdVar dvar = linevardetailMap.get(new FileLineVarDataId(fileID, linenum, fieldname));
-//				Integer count = new Integer(dvar.getCount().intValue() + 1);
-//				List<DataID> dataidlist = dvar.getDataIDList();
-//				dataidlist.add(new DataID(dataid, isPut));
-//				linevardetailMap.put(new FileLineVarDataId(fileID, linenum, fieldname),
-//						new DataIdVar(fieldname, count, dataidlist));
-//			} else {
-//				List<String> varlist = linevarMap.get(new FileLineDataId(fileID, linenum));
-//				varlist.add(fieldname);
-//				linevarMap.put(new FileLineDataId(fileID, linenum), varlist);
-//				List<DataID> dataidlist = new ArrayList<>();
-//				dataidlist.add(new DataID(dataid, isPut));
-//				linevardetailMap.put(new FileLineVarDataId(fileID, linenum, fieldname),
-//						new DataIdVar(fieldname, 1, dataidlist));
-//			}
-//		} else {
-//			List<String> varlist = new ArrayList<>();
-//			varlist.add(fieldname);
-//			linevarMap.put(new FileLineDataId(fileID, linenum), varlist);
-//			List<DataID> dataidlist = new ArrayList<>();
-//			dataidlist.add(new DataID(dataid, isPut));
-//
-//			linevardetailMap.put(new FileLineVarDataId(fileID, linenum, fieldname),
-//					new DataIdVar(fieldname, 1, dataidlist));
-//		}
-//	}
-
-
 
 	public List<String> getLinesRecentdata() {
 		return linesRecentdata;
@@ -125,24 +41,7 @@ public class SeloggerFiles {
 		return linesMethods;
 	}
 
-	public Map<FileLineDataId, List<String>> getLineVarMap() {
-		return linevarMap;
-	}
-
-	public Map<FileLineVarDataId, DataIdVar> getLineVarDetailMap() {
-		return linevardetailMap;
-	}
-
-	public Map<String, String> getFileIDMap() {
-		return fileIDMap;
-	}
-
-	public Map<String, List<String>> getdupFileIDMap() {
-		return dupfileIDMap;
-	}
-
 	public DataIdMaps getDataidMaps() {
 		return dataidMaps;
 	}
-
 }
