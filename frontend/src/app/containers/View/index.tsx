bfda680c46465c8a8fd898eb043db051ca9f7b73
Naoto Ishida <maxfield.walker@gmail.com>
2019-08-23 11:08:45 +0900

    Load json data

--- frontend/src/app/containers/View/index.tsx
+++ frontend/src/app/containers/View/index.tsx
@@ -1,7 +1,17 @@
+import { jsonData } from 'app/models/variable';
 import * as React from 'react';
 
 export class ViewContainer extends React.Component {
+  componentDidMount() {
+    console.log(jsonData);
+  }
   render() {
-    return <div>View component</div>;
+    return (
+      <div>
+        {jsonData.data.map((x) => (
+          <div>{x.var}</div>
+        ))}
+      </div>
+    );
   }
 }
