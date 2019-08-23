bea791f707826c777bca947c6b1129e3151272db
Naoto Ishida <maxfield.walker@gmail.com>
2019-08-23 11:14:58 +0900

    Display source code on view page

--- frontend/src/app/containers/View/index.tsx
+++ frontend/src/app/containers/View/index.tsx
@@ -1,13 +1,15 @@
+import { Sourcecode } from 'app/components/sourcecode';
+import * as JavaLexer from 'app/models/javaLexer';
+import { rawSourceCode } from 'app/models/rawSourceCode';
 import { jsonData } from 'app/models/variable';
 import * as React from 'react';
 
 export class ViewContainer extends React.Component {
-  componentDidMount() {
-    console.log(jsonData);
-  }
   render() {
+    const tokens = JavaLexer.tokenize(rawSourceCode);
     return (
       <div>
+        <Sourcecode tokens={tokens} data={{}} />
         {jsonData.data.map((x) => (
           <div>{x.var}</div>
         ))}
