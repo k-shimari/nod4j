00074e3f5a3740f647d2e987815dd6653ff8048d
k-shimari <k-simari@ist.osaka-u.ac.jp>
2019-08-23 16:23:21 +0900

    add SplitRecentdata method

--- test/data/SplitLineTest.java
+++ test/data/SplitLineTest.java
@@ -9,12 +9,19 @@ public class SplitLineTest {
 	@Test
 	public void splitRecentdataTest() {
 		SplitLine s1 = new SplitLine("1,0,0,2,0,1");
-		SplitLine s2 = new SplitLine("1,0,0,aa\"aa\",0,1");
-		SplitLine s3 = new SplitLine("1,0,0,aa\"a,,a\",0,1");
+		SplitLine s2 = new SplitLine("1,0,0,java.lang.String@11111111:\"abc\",0,1");
+		SplitLine s3 = new SplitLine("1,0,0,java.lang.String@11111111:\"a,b,c\",0,1");
+		SplitLine s4 = new SplitLine("1,0,0,java.lang.String@11111111:\"a,b,c,\\\"\",0,1");
+		SplitLine s5 = new SplitLine("1,0,0,java.lang.String@11111111:\",,,,,\",0,1");
 
 		assertArrayEquals("Error", new String[] { "1", "0", "0", "2", "0", "1" }, s1.getElements());
-		assertArrayEquals("Error", new String[] { "1", "0", "0", "aa\"aa\"", "0", "1" }, s2.getElements());
-		assertArrayEquals("Error", new String[] { "1", "0", "0", "aa\"a,,a\"", "0", "1" }, s3.getElements());
+		assertArrayEquals("Error", new String[] { "1", "0", "0", "java.lang.String@11111111:\"abc\"", "0", "1" }, s2.getElements());
+		assertArrayEquals("Error", new String[] { "1", "0", "0", "java.lang.String@11111111:\"a,b,c\"", "0", "1" }, s3.getElements());
+		assertArrayEquals("Error", new String[] { "1", "0", "0", "java.lang.String@11111111:\"a,b,c,\\\"\"", "0", "1" }, s4.getElements());
+		for(String s:s5.getElements()) {
+			System.out.println(s);
+		}
+		assertArrayEquals("Error", new String[] { "1", "0", "0", "java.lang.String@11111111:\",,,,,\"", "0", "1" }, s5.getElements());
 
 	}
 }
