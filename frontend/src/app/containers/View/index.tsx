fd49307d1892a73d5bc6e135f2147ccba921b064
Naoto Ishida <maxfield.walker@gmail.com>
2019-08-26 23:22:42 +0900

    Wrap source code component with Paper component

--- frontend/src/app/containers/View/index.tsx
+++ frontend/src/app/containers/View/index.tsx
@@ -1,3 +1,4 @@
+import { makeStyles, Paper } from '@material-ui/core';
 import { LogvisActions } from 'app/actions';
 import { ValueListItemData } from 'app/components/organisms/valueList';
 import { Sourcecode } from 'app/components/sourcecode';
@@ -5,9 +6,18 @@ import { RootState } from 'app/reducers';
 import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    margin: theme.spacing(2),
+    padding: theme.spacing(1),
+    width: 720
+  }
+}));
+
 export function ViewContainer() {
   const logvisState = useSelector<RootState, RootState.LogvisState>((state) => state.logvis);
   const dispatch = useDispatch();
+  const classes = useStyles();
 
   React.useEffect(() => {
     dispatch(LogvisActions.requestSourceCodeData({}));
@@ -29,12 +39,14 @@ export function ViewContainer() {
 
   return tokens ? (
     <div>
-      <Sourcecode
-        tokens={tokens}
-        data={filteredValueListData}
-        onArrowUpwardClick={onArrowUpClick}
-        onArrowDownwardClick={onArrowDownClick}
-      />
+      <Paper className={classes.paper}>
+        <Sourcecode
+          tokens={tokens}
+          data={filteredValueListData}
+          onArrowUpwardClick={onArrowUpClick}
+          onArrowDownwardClick={onArrowDownClick}
+        />
+      </Paper>
     </div>
   ) : null;
 }
