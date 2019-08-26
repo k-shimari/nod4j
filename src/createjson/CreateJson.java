330926d3f630b5c41c393b957da465723ef44f2e
k-shimari <k-simari@ist.osaka-u.ac.jp>
2019-08-26 20:23:29 +0900

    separate file for print and modify output

--- src/createjson/CreateJson.java
+++ src/createjson/CreateJson.java
@@ -1,19 +1,11 @@
 package createjson;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import data.Json;
 import data.Recentdata;
@@ -34,7 +26,8 @@ public class CreateJson {
 		System.out.println("Create json ...");
 		List<Json> jsonList = create();
 		try {
-			printJson(jsonList);
+			PrintJson pj =new PrintJson(targetDir,FILENAME);
+			pj.printJson(jsonList);
 			System.out.println("Create json SUCCESS at " + targetDir);
 		} catch (IOException e) {
 			System.err.println("Create json FAILED");
@@ -42,24 +35,6 @@ public class CreateJson {
 		}
 	}
 
-	private void printJson(List<Json> jsonList) throws IOException {
-		ObjectMapper mapper = new ObjectMapper();
-		List<String> lines = jsonList.stream()
-				.map(s -> {
-					try {
-						return mapper.writeValueAsString(s);
-					} catch (JsonProcessingException e) {
-						e.printStackTrace();
-						return "";
-					}
-				})
-				.collect(Collectors.toList());
-		if (Files.exists(Paths.get(targetDir, FILENAME))) {
-			Files.delete(Paths.get(targetDir, FILENAME));
-		}
-		Files.createFile(Paths.get(targetDir, FILENAME));
-		Files.write(Paths.get(targetDir, FILENAME), lines, Charset.forName("UTF-8"), StandardOpenOption.WRITE);
-	}
 
 	private List<Json> create() {
 		List<Json> jsonList = new ArrayList<>();
