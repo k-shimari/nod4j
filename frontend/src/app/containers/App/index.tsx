e1f99bb1bb45ff22c9bc507c5194417e29074e6b
Naoto Ishida <maxfield.walker@gmail.com>
2019-08-28 18:20:40 +0900

    Created top page

--- frontend/src/app/containers/App/index.tsx
+++ frontend/src/app/containers/App/index.tsx
@@ -1,10 +1,41 @@
-import { Link } from '@material-ui/core';
+import { Link, makeStyles, Paper, Typography } from '@material-ui/core';
 import * as React from 'react';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(3, 2)
+  }
+}));
+
 export function App() {
+  const classes = useStyles();
+
   return (
     <div>
-      <Link href="/files">Files</Link>
+      <Paper className={classes.root}>
+        <Typography variant="h5" component="h3">
+          Welcome to LOGVIS
+        </Typography>
+        <Typography component="p">
+          <Link href="/files">Demo</Link>
+        </Typography>
+        <Typography component="p" color="textSecondary">
+          Source code is availabe on{' '}
+          <Link href="https://github.com/k-shimari/LOGVIS" target="_blank">
+            GitHub
+          </Link>
+          .
+        </Typography>
+        <Typography component="p" color="textSecondary">
+          Author:{' '}
+          <Link href="https://github.com/k-shimari" target="_blank">
+            k-shimari
+          </Link>{' '}
+          <Link href="https://github.com/maxfie1d" target="_blank">
+            maxfie1d
+          </Link>
+        </Typography>
+      </Paper>
     </div>
   );
 }
