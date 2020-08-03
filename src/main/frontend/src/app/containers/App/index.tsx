ce7da9729e815e3c0bc6685682f0fa17f24d1823
k-shimari <k-simari@ist.osaka-u.ac.jp>
2020-08-03 13:03:16 +0900

    edit toppage for sample

--- src/main/frontend/src/app/containers/App/index.tsx
+++ src/main/frontend/src/app/containers/App/index.tsx
@@ -100,66 +100,20 @@ function OpenProjectPanel() {
       </Typography>
       <div>
         <List dense>
-          {projects
-            ? projects.map((item, index) => <ProjectListItem key={index} {...item} />)
-            : null}
+        <ListItem button component="a" href={`project/demo/files`}>
+            <ListItemText
+              key={-1}
+              primary="Demo"
+              secondary="This is the demo project."
+              primaryTypographyProps={{ color: 'primary' }}
+            />
+          </ListItem>
         </List>
       </div>
-      <Box display="flex" alignItems="center" mt={1} mb={1}>
-        <Box flexGrow={1}>
-          <TextField
-            margin="dense"
-            variant="outlined"
-            fullWidth
-            placeholder="Your project name"
-            value={addProjectName}
-            onChange={(e) => setAddProejctName(e.target.value)}
-          />
-        </Box>
-        <Box ml={2}>
-          <Button
-            onClick={() => addProject(addProjectName)}
-            disabled={!addProjectName}
-            variant="contained"
-            color={addProjectName ? 'primary' : 'default'}
-          >
-            Add project
-          </Button>
-        </Box>
-      </Box>
-      <Box mt={1}>
-        <Typography variant="caption">
-          <MULink href="https://github.com/k-shimari/nod4j" target="_blank"> 
-          You can check how to import your project here.
-          </MULink>
-        </Typography>
-      </Box>
     </Paper>
   );
 }
 
-function DebugPanel() {
-  const classes = useStyles();
-  const dispatch = useDispatch();
-
-  return (
-    <Paper className={classes.root}>
-      <Typography variant="h5" component="h3" gutterBottom>
-        Debug
-      </Typography>
-      <div>
-        <Button
-          variant="outlined"
-          size="small"
-          onClick={() => dispatch(nod4jActions.clearLocalStorage())}
-        >
-          Clear Timestamp Filter
-          <DeleteIcon />
-        </Button>
-      </div>
-    </Paper>
-  );
-}
 
 export function App() {
   return (
@@ -168,9 +122,6 @@ export function App() {
       <Grid container spacing={3}>
         <Grid item xs={6}>
           <OpenProjectPanel />
-        </Grid>
-        <Grid item xs={6}>
-          <DebugPanel />
         </Grid>
       </Grid>
     </ContentContainer>
