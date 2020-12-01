2495753b1f05ffa61128d4d66c06b6ee08ac1f74
k-shimari <k-simari@ist.osaka-u.ac.jp>
2020-12-01 15:19:07 +0900

    update comments

--- src/main/frontend/scripts/server.ts
+++ src/main/frontend/scripts/server.ts
@@ -1,16 +1,20 @@
 import * as Express from 'express';
 import * as path from 'path';
-const history = require('connect-history-api-fallback');
 
+/*
+ * Define the buildpath and send index.html.
+ */
+const history = require('connect-history-api-fallback');
 const app = Express();
 app.use(history());
-
 app.use(Express.static(path.join(__dirname, '../build')));
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
+/*
+ * Define the port for running server.
+ */
 app.listen(8070, () => {
   console.log('Server running');
 });
