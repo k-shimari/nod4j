248798d9a98644f4710850d5b25d42014a33646a
KANDA, Tetsuya <t-kanda@ist.osaka-u.ac.jp>
2021-10-18 13:37:30 +0900

    method name matching

--- src/main/java/jp/ac/osaka_u/ist/sel/nod4j/data/methodparam/AddParam.java
+++ src/main/java/jp/ac/osaka_u/ist/sel/nod4j/data/methodparam/AddParam.java
@@ -11,108 +11,107 @@ import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Stack;
 
 /**
  * Get argument values
  *
  * @author k-simari
+ * @author t-kanda
  */
 public class AddParam {
-    /**
-     * This function returns the information of the method parameters in the specified file.
-     *
-     * @param f path + filename (e.g., src/main/filename.java)
-     */
-    public Map<String, List<ParamInfo>> getParamInfo(File f, String filePath) {
-        try {
-            CompilationUnit unit = StaticJavaParser.parse(f);
-            MethodParamVisitor v = new MethodParamVisitor();
-            unit.accept(v, new CM(filePath));
+	/**
+	 * This function returns the information of the method parameters in the specified file.
+	 *
+	 * @param f path + filename (e.g., src/main/filename.java)
+	 */
+	public Map<String, ParamInfo> getParamInfo(File f, String filePath) {
+		try {
+			CompilationUnit unit = StaticJavaParser.parse(f);
+			MethodParamVisitor v = new MethodParamVisitor();
+			unit.accept(v, new CM(filePath));
 
-            return v.methodParams;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return new HashMap<>();
-        }
-    }
+			return v.methodParams;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return new HashMap<>();
+		}
+	}
 
-    public class MethodParamVisitor extends VoidVisitorAdapter<CM> {
+	public class MethodParamVisitor extends VoidVisitorAdapter<CM> {
 
-        private Map<String, List<ParamInfo>> methodParams = new HashMap<>();
+		private Map<String, ParamInfo> methodParams = new HashMap<>();
 
-        private void addParams(String className, ParamInfo pi) {
-            List<ParamInfo> tmp = methodParams.getOrDefault(className, new ArrayList<>());
-            // System.out.println("P " + className + " " + pi.getMethodName() + " " + pi.getArgumentName() + " " + pi.getLine());
-            tmp.add(pi);
-            methodParams.put(className, tmp);
-        }
+		private void addParams(CM cm, String argumentName, String type, int line) {
+			ParamInfo tmp = methodParams.getOrDefault(cm.getClassName(), new ParamInfo());
+			// System.out.println("P " + cm.getClassName() + " " + cm.currentMethodName + " " + argumentName + " " + line);
+			tmp.put(cm.currentMethodName, argumentName, type, line);
+			methodParams.put(cm.getClassName(), tmp);
+		}
 
-        @Override
-        public void visit(ClassOrInterfaceDeclaration n, CM arg) {
-            arg.currentClassName.push(n.getNameAsString());
-            // System.out.println("C " + arg);
-            super.visit(n, arg);
-            arg.currentClassName.pop();
-        }
+		@Override
+		public void visit(ClassOrInterfaceDeclaration n, CM arg) {
+			arg.currentClassName.push(n.getNameAsString());
+			// System.out.println("C " + arg);
+			super.visit(n, arg);
+			arg.currentClassName.pop();
+		}
 
-        @Override
-        public void visit(MethodDeclaration n, CM arg) {
-            arg.currentMethodName = n.getNameAsString();
-            // System.out.println("M " + arg);
-            for (Parameter p :n.getParameters()){
-                addParams(arg.getClassName(), new ParamInfo(n.getNameAsString(), p.getNameAsString(), p.getTypeAsString(), p.getBegin().get().line));
-            }
-            super.visit(n, arg);
-            arg.currentMethodName = "";
-        }
+		@Override
+		public void visit(MethodDeclaration n, CM arg) {
+			arg.currentMethodName = n.getNameAsString();
+			// System.out.println("M " + arg);
+			for (Parameter p : n.getParameters()) {
+				addParams(arg, p.getNameAsString(), p.getTypeAsString(), p.getBegin().get().line);
+			}
+			super.visit(n, arg);
+			arg.currentMethodName = "";
+		}
 
-        @Override
-        public void visit(ConstructorDeclaration n, CM arg) {
-            arg.currentMethodName = "<init>";
-            for (Parameter p :n.getParameters()){
-                addParams(arg.getClassName(), new ParamInfo(n.getNameAsString(), p.getNameAsString(), p.getTypeAsString(), p.getBegin().get().line));
-            }
-            // System.out.println("m " + arg);
-            super.visit(n, arg);
-            arg.currentMethodName = "";
-        }
+		@Override
+		public void visit(ConstructorDeclaration n, CM arg) {
+			arg.currentMethodName = "<init>";
+			for (Parameter p : n.getParameters()) {
+				addParams(arg, p.getNameAsString(), p.getTypeAsString(), p.getBegin().get().line);
+			}
+			// System.out.println("m " + arg);
+			super.visit(n, arg);
+			arg.currentMethodName = "";
+		}
 
-        @Override
-        public void visit(ObjectCreationExpr n, CM arg) {
-            if (n.getAnonymousClassBody().isPresent()) {
-                arg.currentClassName.push(Integer.toString(arg.anonymous++));
-                // System.out.println("A " + arg);
-                super.visit(n, arg);
-                arg.currentClassName.pop();
-            } else {
-                super.visit(n, arg);
-            }
-        }
-    }
+		@Override
+		public void visit(ObjectCreationExpr n, CM arg) {
+			if (n.getAnonymousClassBody().isPresent()) {
+				arg.currentClassName.push(Integer.toString(arg.anonymous++));
+				// System.out.println("A " + arg);
+				super.visit(n, arg);
+				arg.currentClassName.pop();
+			} else {
+				super.visit(n, arg);
+			}
+		}
+	}
 
-    private class CM {
+	private class CM {
 
-        String path;
-        String currentMethodName = "";
-        Stack<String> currentClassName = new Stack<>();
-        int anonymous = 1;
+		String path;
+		String currentMethodName = "";
+		Stack<String> currentClassName = new Stack<>();
+		int anonymous = 1;
 
-        public CM(String filePath) {
-            path = filePath.substring(0, filePath.lastIndexOf("/") + 1);
-        }
+		public CM(String filePath) {
+			path = filePath.substring(0, filePath.lastIndexOf("/") + 1);
+		}
 
-        @Override
-        public String toString() {
-            return getClassName() + ' ' + currentMethodName;
-        }
+		@Override
+		public String toString() {
+			return getClassName() + ' ' + currentMethodName;
+		}
 
-        public String getClassName() {
-            return path + String.join("$", currentClassName);
-        }
-    }
+		public String getClassName() {
+			return path + String.join("$", currentClassName);
+		}
+	}
 }
--- src/main/java/jp/ac/osaka_u/ist/sel/nod4j/data/methodparam/MethodParam.java
+++ src/main/java/jp/ac/osaka_u/ist/sel/nod4j/data/methodparam/MethodParam.java
@@ -1,5 +1,7 @@
 package jp.ac.osaka_u.ist.sel.nod4j.data.methodparam;
 
+import jp.ac.osaka_u.ist.sel.nod4j.data.methodparam.ParamInfo.ParamInfoInner;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,6 +13,7 @@ import java.util.Map;
 
 /**
  * add method parameter information to dataids
+ *
  * @author k-simari
  */
 public class MethodParam {
@@ -23,10 +26,10 @@ public class MethodParam {
 	 */
 	private String traceDir;
 
-	private Map<String, List<ParamInfo>> fileMethodParamMap;
-	private Map<String, String> classIDClassMap;
+	private Map<String, ParamInfo> fileMethodParamMap;
+	private Map<String, String> classIDClassMap, methodIDMethodMap;
 
-	public MethodParam(String projectDir, String traceDir, Map<String, String> classIDClassMap) {
+	public MethodParam(String projectDir, String traceDir, Map<String, String> classIDClassMap, Map<String, String> methodIDMethodMap) {
 		//if (dir.endsWith(System.getProperty("file.separator")))
 		/* when path finish /or\ remove it.*/
 		if (projectDir.endsWith("\\") || projectDir.endsWith("/"))
@@ -37,6 +40,7 @@ public class MethodParam {
 		this.projectDir = projectDir;
 		this.fileMethodParamMap = new HashMap<>();
 		this.classIDClassMap = classIDClassMap;
+		this.methodIDMethodMap = methodIDMethodMap;
 	}
 
 	/**
@@ -50,6 +54,7 @@ public class MethodParam {
 	/**
 	 * This fuction maps the actual filepath to filepath recorded in the trace.
 	 * This method replaces the path (e.g., src/main/java) because recorded path omits such part of the path.
+	 *
 	 * @param dir is the specific directory of the target project.
 	 */
 	private void getDirInfo(File dir) {
@@ -59,20 +64,20 @@ public class MethodParam {
 				try {
 					if (f.isFile()) {
 						if (f.getName().length() >= 6
-								&& f.getName().substring(f.getName().length() - 5).equals(".java")) {
+							  && f.getName().substring(f.getName().length() - 5).equals(".java")) {
 							//@TODO edit hashmap key
 							String packageName = f.getParent().replace("\\", "/");
 							String filePath = packageName.replace(projectDir.replace("\\", "/") + "/", "")
-									.replaceFirst("^src\\/main\\/java\\/", "")
-									.replaceFirst("^src\\/test\\/java\\/", "")
-									.replaceFirst("^test\\/main\\/java\\/", "")
-									.replaceFirst("^tests\\/main\\/java\\/", "")
-									.replaceFirst("^source\\/", "")
-									.replaceFirst("^sources\\/", "")
-									.replaceFirst("^src\\/", "")
-									.replaceFirst("^test\\/", "")
-									.replaceFirst("^tests\\/", "")
-									+ "/" + f.getName().replace(".java", "");
+								  .replaceFirst("^src\\/main\\/java\\/", "")
+								  .replaceFirst("^src\\/test\\/java\\/", "")
+								  .replaceFirst("^test\\/main\\/java\\/", "")
+								  .replaceFirst("^tests\\/main\\/java\\/", "")
+								  .replaceFirst("^source\\/", "")
+								  .replaceFirst("^sources\\/", "")
+								  .replaceFirst("^src\\/", "")
+								  .replaceFirst("^test\\/", "")
+								  .replaceFirst("^tests\\/", "")
+								  + "/" + f.getName().replace(".java", "");
 							System.out.println("filePath:::" + filePath);
 							this.fileMethodParamMap.putAll(getFileInfo(f, filePath));
 						}
@@ -86,7 +91,7 @@ public class MethodParam {
 		}
 	}
 
-	private Map<String, List<ParamInfo>> getFileInfo(File f, String filePath) throws IOException {
+	private Map<String, ParamInfo> getFileInfo(File f, String filePath) throws IOException {
 		AddParam a = new AddParam();
 		return a.getParamInfo(f, filePath);
 	}
@@ -113,6 +118,7 @@ public class MethodParam {
 
 	/**
 	 * This method adds parameter name to dataids element.
+	 *
 	 * @param elem is the parsing result of "dataids.txt"
 	 */
 	private void rewriteLine(String[] elem) {
@@ -120,11 +126,18 @@ public class MethodParam {
 			String classname = classIDClassMap.get(elem[1]);
 			System.out.println("class:::" + classname);
 			if (fileMethodParamMap.containsKey(classname)) {
-				List<ParamInfo> list = fileMethodParamMap.get(classname);
-				if (!list.isEmpty()) {
-					elem[3] = String.valueOf(list.get(0).getLine());
-					elem[5] += ",ParamName=" + list.get(0).getArgumentName();
-					list.remove(0);
+				ParamInfo paramInfo = fileMethodParamMap.get(classname);
+				String methodName = methodIDMethodMap.get(elem[2]);
+				/* note: overridden method will appear right after overriding method
+				 (... overriding method id is younger than overridden method id ...)
+				 so maybe here is safe.
+				 */
+				// parameters are stored in order of appearance
+				ParamInfoInner params = paramInfo.get(methodName);
+				if (params != null) {
+					System.out.println(params.getArgumentName() + " " + params.getLine());
+					elem[3] = String.valueOf(params.getLine());
+					elem[5] += ",ParamName=" + params.getArgumentName();
 				}
 			}
 		}
